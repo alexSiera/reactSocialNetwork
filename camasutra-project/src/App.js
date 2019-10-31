4f8bfff0ed18b19d24a73c0828513809a384c34f
alexSiera <fa1nt.siera@gmail.com>
2019-10-31 23:42:54 +0300

    Add new project

---
+++ camasutra-project/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>Css</li>
+        <li>Html</li>
+
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
