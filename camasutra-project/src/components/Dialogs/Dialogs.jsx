3a2c79f77c252711e6505280b668286358cd68c4
sieraAlex <fa1nt.siera@gmail.com>
2020-01-01 21:22:51 +0300

    start styling dialogs page

--- camasutra-project/src/components/Dialogs/Dialogs.jsx
+++ camasutra-project/src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 import s from './Dialogs.module.scss';
 const Dialogs = (props) => {
     return (
-        <div >
+        <div className={s.dialogs} >
             <div className={s.dialogs_items}>
-                <div>
-
+                <div className={s.dialog}>
+                    Dimych
                 </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+
+                <div className={s.message}>YO</div>
+
             </div>
         </div>
     )
