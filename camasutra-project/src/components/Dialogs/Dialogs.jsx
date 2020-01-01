af658c160d4980d8840a45df116c4d5a26541f4f
sieraAlex <fa1nt.siera@gmail.com>
2020-01-01 23:26:19 +0300

    rework Dialogs with map

--- camasutra-project/src/components/Dialogs/Dialogs.jsx
+++ camasutra-project/src/components/Dialogs/Dialogs.jsx
@@ -1,34 +1,66 @@
 import React from 'react';
 import s from './Dialogs.module.scss';
 import {NavLink} from "react-router-dom";
+
 const DialogItem = ({linkText, id}) => {
     let path = '/dialogs/' + id;
     return (
         <div className={`${s.dialog} ${s.active}`}>
-            <NavLink to={path}>{linkText}</NavLink>
+            <NavLink id={id} to={path}>{linkText}</NavLink>
         </div>
     )
 }
-const Message = ({message}) => {
+const Message = ({message, id, likesCount}) => {
     return (
-        <div className={s.dialog}>{message}</div>
+        <div className={s.dialog} id={id} >{message}</div>
     )
 }
 const Dialogs = (props) => {
+    const dialogsData = [{
+        id: 1,
+        name: "Dimych"
+    }, {
+        id: 2,
+        name: "Alex"
+    }, {
+        id: 3,
+        name: "Anya"
+    }, {
+        id: 4,
+        name: "Killer"
+    }];
+    const messagesData = [{
+        id: 1,
+        message: "Ok!",
+        likesCount: 12,
+    }, {
+        id: 2,
+        message: "Sure!",
+        likesCount: 12,
+    }, {
+        id: 3,
+        message: "Hi dos",
+        likesCount: 12,
+    }, {
+        id: 4,
+        message: "Killer",
+        likesCount: 12,
+    }];
     return (
-        <div className={s.dialogs} >
+        <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-                <DialogItem linkText="Dimych" id="1" />
-                <DialogItem linkText="Alex" id="2"  />
-                <DialogItem linkText="Anya" id="3"  />
-                <DialogItem linkText="Killer" id="4"  />
+                {dialogsData.map((el) => {
+                    return <DialogItem id={el.id} linkText={el.name} />
+                })}
+                {/*<DialogItem linkText={dialogsData[1].name} id={dialogsData[1].id}/>*/}
+                {/*<DialogItem linkText={dialogsData[2].name} id={dialogsData[2].id}/>*/}
+                {/*<DialogItem linkText={dialogsData[3].name} id={dialogsData[3].id}/>*/}
 
             </div>
             <div className={s.messages}>
-                <Message message="Ok!" />
-                <Message message="Ok 2!" />
-                <Message message="Ok 3!" />
-
+                {messagesData.map((el) => {
+                    return <Message id={el.id} message={el.message} likesCount={el.likesCount}/>
+                })}
             </div>
         </div>
     )
