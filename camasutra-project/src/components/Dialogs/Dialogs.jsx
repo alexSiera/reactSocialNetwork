c85b126c76f17db9a3720229b3ce90ca75bede46
sieraAlex <fa1nt.siera@gmail.com>
2020-01-02 17:07:51 +0300

    work with reference

--- camasutra-project/src/components/Dialogs/Dialogs.jsx
+++ camasutra-project/src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import s from './Dialogs.module.scss';
-import {NavLink} from "react-router-dom";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
@@ -12,10 +11,6 @@ const Dialogs = (props) => {
                 {props.dialogsDatas.map((el) => {
                     return <DialogItem id={el.id} linkText={el.name} />
                 })}
-                {/*<DialogItem linkText={dialogsData[1].name} id={dialogsData[1].id}/>*/}
-                {/*<DialogItem linkText={dialogsData[2].name} id={dialogsData[2].id}/>*/}
-                {/*<DialogItem linkText={dialogsData[3].name} id={dialogsData[3].id}/>*/}
-
             </div>
             <div className={s.messages}>
                 {props.messagesDatas.map((el) => {
--- camasutra-project/src/components/Profile/MyPosts/MyPosts.jsx
+++ camasutra-project/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,25 @@ import s from './MyPosts.module.scss';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-    let postsElements = props.postsData.map((p => <Post id={p.id} message={p.message} likesCount={p.likesCount} />))
+    const postsElements = props.postsData.map((p => <Post id={p.id} message={p.message} likesCount={p.likesCount} />))
+    const newPostElement = React.createRef()
+    const addPost = () => {
+        debugger;
+        const text = newPostElement.current.value;
+        console.log(newPostElement)
+        alert(text)
+    }
     return (
         <div className={s.postsBlock}>
             <h3>My post</h3>
             <div className={s.postsBlockText}>
-                <div><textarea></textarea></div>
                 <div>
-                    <button>Add post</button>
+                    <textarea ref={newPostElement}>
+
+                    </textarea>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
