2570952b2459ee88e7e04003405a260d0b3f166d
sieraAlex <fa1nt.siera@gmail.com>
2020-01-05 13:20:25 +0300

    add CONTEXT PROVIDER component

--- camasutra-project/src/StoreContext.js
+++ camasutra-project/src/StoreContext.js
@@ -1,4 +1,10 @@
 import React from "react";
+import store from "./Redux/reduxStore";
 
 const StoreContext = React.createContext(null);
+export const Provider = (props) => {
+    return(<StoreContext.Provider value={props.store}>
+        {props.children}
+    </StoreContext.Provider>)
+}
 export default StoreContext;
--- camasutra-project/src/index.js
+++ camasutra-project/src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from "./Redux/reduxStore";
 import {BrowserRouter} from "react-router-dom";
-import StoreContext from "./StoreContext";
+import {Provider} from "./StoreContext";
  // let {getState} = store;
  // let state = getState();
 const rerenderEntireTree = () => {
     ReactDOM.render(
             <BrowserRouter>
-                <StoreContext.Provider value={store}>
+                <Provider store={store}>
                     <App />
-                </StoreContext.Provider>
+                </Provider>
             </BrowserRouter>
 , document.getElementById('root'));
 }
