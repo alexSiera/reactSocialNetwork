16568a426fdc0f45f7a1916e5be0e61a6cc6e35c
sieraAlex <fa1nt.siera@gmail.com>
2020-01-20 20:23:04 +0300

    start doing reselect 2

--- camasutra-project/src/Redux/reducers/usersSelectors.js
+++ camasutra-project/src/Redux/reducers/usersSelectors.js
@@ -1,6 +1,7 @@
 export const getUsers= (state) => {
     return state.usersPage.users;
 };
+
 export const getPageSize= (state) => {
     return state.usersPage.pageSize;
 };
@@ -16,3 +17,12 @@ export const getIsFetching= (state) => {
 export const getFollowingInProgress= (state) => {
     return state.usersPage.followingInProgress;
 };
+// export const countSomethingDifficult= (state) => {
+//     //for... math... big array
+//     let count = 23;
+//     return count;
+//     return state.usersPage.followingInProgress;
+// };
+// export const getTempUsers= (state) => {
+//     return state.usersPage.users.filter(u => true //that are in temperedArray);
+// };
