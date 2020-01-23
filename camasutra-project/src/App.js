7453da4b2655c311b9a2e79034f0e602c18288c8
sieraAlex <fa1nt.siera@gmail.com>
2020-01-23 17:48:28 +0300

    add suspence and lazy load

--- camasutra-project/src/App.js
+++ camasutra-project/src/App.js
@@ -1,23 +1,23 @@
-import React, {useEffect} from "react";
+import React, {useEffect, lazy, Suspense} from "react";
 import "./App.scss";
-import {Route, withRouter} from "react-router-dom";
-import {connect} from "react-redux";
+import Preloader from "./components/Common/Preloader/Preloader";
+import store from "./Redux/reduxStore";
 import {compose} from 'redux';
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import NewsContainer from "./components/News/NewsContainer";
-import Navbar from "./components/Navbar/Navbar";
+import {connect, Provider} from "react-redux";
+import {Route, withRouter, BrowserRouter} from "react-router-dom";
+import {initializeApp} from "./Redux/reducers/appReducer";
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginContainer from "./components/Login/LoginContainer";
-import {initializeApp} from "./Redux/reducers/appReducer";
-import Preloader from "./components/Common/Preloader/Preloader";
-import ProfileContainerWithHooks from "./components/Profile/ProfileContainerWithHooks";
-import store from "./Redux/reduxStore";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-
+//import ProfileContainerWithHooks from "./components/Profile/ProfileContainerWithHooks";
+import Navbar from "./components/Navbar/Navbar";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+//import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import NewsContainer from "./components/News/NewsContainer";
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+const NewsContainer = lazy(() => import('./components/News/NewsContainer'));
+const ProfileContainerWithHooks = lazy(() => import('./components/Profile/ProfileContainerWithHooks'));
 const App = ({initializeApp, initialized}) => {
     useEffect(() => {
             initializeApp();
@@ -29,16 +29,38 @@ const App = ({initializeApp, initialized}) => {
             <HeaderContainer/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-                <Route path="/profile/:userId?" render={() => <ProfileContainerWithHooks/>}/>
+                <Route path="/dialogs" render={() => {
+                    return (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <DialogsContainer/>
+                        </Suspense>
+                    )
+                }
+                }/>
+                <Route path="/profile/:userId?" render={() => {
+                    return (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <ProfileContainerWithHooks/>
+                        </Suspense>
+                    )
+                }
+                }/>
                 <Route path="/users" render={() => <UsersContainer/>}/>
                 <Route path="/music" component={Music}/>
-                <Route path="/news" render={() => <NewsContainer/>}/>
+                <Route path="/news" render={() => {
+                    return (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <NewsContainer/>
+                        </Suspense>
+                    )
+                }
+                }/>
                 <Route path="/settings" component={Settings}/>
                 <Route path="/login" render={() => <LoginContainer/>}/>
             </div>
         </div>
-    );
+    )
+        ;
 };
 const mapStateToProps = ({app}) => ({initialized: app.initialized});
 const AppContainer = compose(
