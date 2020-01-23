7ebdd86115e37efcb0a3e0953036e9c327ba20a6
sieraAlex <fa1nt.siera@gmail.com>
2020-01-23 22:43:38 +0300

    add basename to browserRouter

--- camasutra-project/src/App.js
+++ camasutra-project/src/App.js
@@ -50,7 +50,7 @@ const AppContainer = compose(
 )(App);
 const SamuraiJSApp = (props) => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <AppContainer/>
             </Provider>
