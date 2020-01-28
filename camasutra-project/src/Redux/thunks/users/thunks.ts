173174e5fa6c54e0e1bf68c966237398a0a6e3b6
sieraAlex <fa1nt.siera@gmail.com>
2020-01-28 19:19:07 +0300

    start adding typescript to thunks 2

--- camasutra-project/src/Redux/thunks/users/thunks.ts
+++ camasutra-project/src/Redux/thunks/users/thunks.ts
@@ -1,5 +1,15 @@
+import {
+    followAC,
+    selectPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    toggleFetchingAC,
+    toggleFollowingProgressAC, unfollowAC
+} from "../../reducers/users/actions";
+import {usersAPI} from "../../../api/api";
+import {ActionCreator, Dispatch} from "redux";
 
-export const requestUsersThunkCreator = (page = 1, pageSize = 4) => async (dispatch) => {
+export const requestUsersThunkCreator = (page = 1, pageSize = 4) => async (dispatch: Dispatch) => {
     dispatch(selectPageAC(page));
     dispatch(toggleFetchingAC(true));
     try {
@@ -11,10 +21,11 @@ export const requestUsersThunkCreator = (page = 1, pageSize = 4) => async (dispa
         console.log(e);
     }
 };
-const followUnfollowFlow = async (dispatch, userId, apiMethod , actionCreator) => {
+const followUnfollowFlow = async (dispatch: Dispatch, userId: number, apiMethod: (userId: number) => void, actionCreator: ActionCreator<any>) => {
     try {
         dispatch(toggleFollowingProgressAC(userId, true));
         const statusCode = await apiMethod(userId);
+        // @ts-ignore
         if (statusCode === 0) dispatch(actionCreator(userId));
         dispatch(toggleFollowingProgressAC(userId, false));
     } catch (e) {
@@ -22,14 +33,14 @@ const followUnfollowFlow = async (dispatch, userId, apiMethod , actionCreator) =
     }
 };
 
-export const followThunkCreator = (userId) => async (dispatch) => {
+export const followThunkCreator = (userId: number) => async (dispatch: Dispatch) => {
     try {
         followUnfollowFlow(dispatch,userId,usersAPI.subscribe.bind(usersAPI),followAC);
     } catch (e) {
         console.log(e);
     }
 };
-export const unFollowThunkCreator = (userId) => async (dispatch) => {
+export const unFollowThunkCreator = (userId: number) => async (dispatch: Dispatch) => {
     try {
         followUnfollowFlow(dispatch,userId,usersAPI.unSubscribe.bind(usersAPI),unfollowAC);
 
