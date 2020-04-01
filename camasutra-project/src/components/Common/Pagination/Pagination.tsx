66c4bc9a1228011621b206e1cbdc2bcbbfed0b47
alexsandrPavlovtsev <alexandr.pavlovtsev@gmail.com>
2020-04-01 21:48:57 +0300

    add prettierc and edit eslintrc file

--- camasutra-project/src/components/Common/Pagination/Pagination.tsx
+++ camasutra-project/src/components/Common/Pagination/Pagination.tsx
@@ -1,60 +1,79 @@
-import React, {useState} from 'react'
-import s from "./Pagination.module.scss";
+import React, { useState } from 'react';
+import s from './Pagination.module.scss';
 import cn from 'classnames';
 
 type PropsType = {
-    onPageChanged: () => void
-    currentSelectedPage: number
-    totalItemsCount: number
-    pageSize: number
-    portionSize: number
-}
+    onPageChanged: () => void;
+    currentSelectedPage: number;
+    totalItemsCount: number;
+    pageSize: number;
+    portionSize: number;
+};
 
-const Pagination: React.FC<PropsType> = ({onPageChanged, currentSelectedPage,
-                                             totalItemsCount, pageSize,
-                                             portionSize = 10}) => {
+const Pagination: React.FC<PropsType> = ({
+    onPageChanged,
+    currentSelectedPage,
+    totalItemsCount,
+    pageSize,
+    portionSize = 10,
+}) => {
     const pagesCount = Math.ceil(totalItemsCount / pageSize);
     const pages = [];
     for (let i = 1; i < pagesCount; i++) {
         pages.push(i);
     }
     const portionCount = Math.ceil(pagesCount / portionSize);
-    let [portionNumber, setPortionNumber] = useState(1);
+    const [portionNumber, setPortionNumber] = useState(1);
     const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
     const rightPortionPageNumber = portionNumber * portionSize;
     return (
         <div className={s.paginationBlock}>
             <div className={cn(s.paginator)}>
-                {portionNumber > 1 &&
-                <button onClick={() => {
-                    setPortionNumber(portionNumber - 1)
-                }}>PREV</button>}
-                {
-                    pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber).map((p) => {
+                {portionNumber > 1 && (
+                    <button
+                        onClick={() => {
+                            setPortionNumber(portionNumber - 1);
+                        }}
+                    >
+                        PREV
+                    </button>
+                )}
+                {pages
+                    .filter((p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                    .map((p) => {
                         return (
-                            <PaginationElement id={p}
-                                               key={p} onPageChanged={onPageChanged}
-                                               currentSelectedPage={currentSelectedPage}
+                            <PaginationElement
+                                id={p}
+                                key={p}
+                                onPageChanged={onPageChanged}
+                                currentSelectedPage={currentSelectedPage}
                             />
-                        )
-                    })
-                }
-                {portionCount > portionNumber &&
-                <button onClick={() => {
-                    setPortionNumber(portionNumber + 1)
-                }}>NEXT</button>}
+                        );
+                    })}
+                {portionCount > portionNumber && (
+                    <button
+                        onClick={() => {
+                            setPortionNumber(portionNumber + 1);
+                        }}
+                    >
+                        NEXT
+                    </button>
+                )}
             </div>
         </div>
-    )
+    );
 };
 
-const PaginationElement = ({id, onPageChanged, currentSelectedPage}) => {
+const PaginationElement = ({ id, onPageChanged, currentSelectedPage }) => {
     return (
-        <span key={id} id={id} onClick={() => onPageChanged(id)}
-              className={cn({[s.selectedPage]: currentSelectedPage === id}, s.pageNum)}>
-        {id}
+        <span
+            key={id}
+            id={id}
+            onClick={() => onPageChanged(id)}
+            className={cn({ [s.selectedPage]: currentSelectedPage === id }, s.pageNum)}
+        >
+            {id}
         </span>
-    )
-
-}
+    );
+};
 export default Pagination;
