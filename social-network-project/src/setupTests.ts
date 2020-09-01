925c177d352bd591652ede4d123bf66fd1353e7f
alexsandrPavlovtsev <alexandr.pavlovtsev@gmail.com>
2020-09-01 16:48:03 +0300

    add jest/enzyme testing package

---
+++ social-network-project/src/setupTests.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
+// Fail tests on any warning
+console.error = (message: string) => {
+  throw new Error(message);
+};
